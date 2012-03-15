8f564e4d130b30fec4172dfbdd72ad97706e602e
nike-17 <nike-17@ya.ru>
2012-03-15 17:51:15 +0700

    cookie imrovement

---
+++ classes/cookie.php
@@ -0,0 +1,13 @@
+<?php
+
+class Cookie extends Kohana_Cookie {
+		
+	public static function set($name, $value, $expiration = NULL) {
+		if (Kohana::$environment == Kohana::TESTING) {
+			Request::initial()->cookie($name, $value);
+		} else {
+			return parent::set($name, $value, $expiration);
+		}
+	}
+
+}
